0ff1856586d8572dc6a66ed6732e709de1bd9127
Pankaj Kumar Gautam <pnkjgautam05@gmail.com>
2019-12-28 19:00:29 +0530

    new bot

--- index.php
+++ index.php
@@ -10,24 +10,17 @@
     
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/jquery-ui.css">
-    
-    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
-
-    
 
     <link rel="stylesheet" href="css/aos.css">
-
-    <link rel="stylesheet" href="css/style.css">
-    
+    <link rel="stylesheet" href="css/style.css">    
   </head>
 
+<!-- This site is converting visitors into subscribers and customers with  - http://app.rocketbots.io 
+dws63526@zzrgg.com-->
 
-<!-- This site is converting visitors into subscribers and customers with  - http://app.rocketbots.io -->
 <script id="rocketbots__widget" src="https://rocketbots.oss-cn-hongkong.aliyuncs.com/webchat/widget/widget.js?cId=411d232109d7acf3b5286e5e4e20fe022f1ea8a61f9a97b6ec13b4bbd281d180"></script>
-<!-- http://app.rocketbots.io -->
 
-
-  <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+<body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
   
   <div class="site-wrap"  id="home-section">
 
@@ -43,7 +36,7 @@
     <div class="container d-none d-lg-block">
       <div class="row">
         <div class="col-12 text-center mb-4 mt-5">
-            <h1 class="mb-0 site-logo"><a href="index.html" class="text-black h2 mb-0">Tinkering with paper circuit<span class="text-primary">.</span> </a></h1>
+            <h1 class="mb-0 site-logo"><a href="index.php" class="text-black h2 mb-0">Tinkering with paper circuit<span class="text-primary">.</span> </a></h1>
           </div>
       </div>
     </div>
@@ -53,7 +46,7 @@
         <div class="row align-items-center">
           
           <div class="col-6 col-md-6 col-xl-2  d-block d-lg-none">
-            <h1 class="mb-0 site-logo"><a href="index.html" class="text-black h2 mb-0">Tinkering with paper circuit<span class="text-primary">.</span> </a></h1>
+            <h1 class="mb-0 site-logo"><a href="index.php" class="text-black h2 mb-0">Tinkering with paper circuit<span class="text-primary">.</span> </a></h1>
           </div>
 
           <div class="col-12 col-md-10 main-menu">
@@ -186,7 +179,7 @@
         <div class="row">
           <div class="col-md-12">
             <p class="mb-0">
-              Made with <i class="icon-heart" aria-hidden="true"></i> by Tinkering With Paper Circuit
+              Tinkering With Paper Circuit | Paperpanks
             </p>
           </div>
         </div>
@@ -196,24 +189,6 @@
   
      
     
-  </div> <!-- .site-wrap -->
-
-  <script src="js/jquery-3.3.1.min.js"></script>
-  <script src="js/jquery-migrate-3.0.1.min.js"></script>
-  <script src="js/jquery-ui.js"></script>
-  <script src="js/popper.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/owl.carousel.min.js"></script>
-  <script src="js/jquery.stellar.min.js"></script>
-  <script src="js/jquery.countdown.min.js"></script>
-  <script src="js/bootstrap-datepicker.min.js"></script>
-  <script src="js/jquery.easing.1.3.js"></script>
-  <script src="js/aos.js"></script>
-  <script src="js/jquery.fancybox.min.js"></script>
-  <script src="js/jquery.sticky.js"></script>
-
-  
-  <script src="js/main.js"></script>
-    
+  </div> <!-- .site-wrap -->    
   </body>
 </html>
